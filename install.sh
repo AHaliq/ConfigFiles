ab0be708c03c79be6594c488a167dc8cfa96249a
AHaliq <liqo12@hotmail.com>
2017-09-15 20:59:11 +0800

    vim plug

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 BASEDIR=$(cd $(dirname "$0") && pwd -P)
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s "$BASEDIR/.vimrc" ~/.vimrc
 ln -s "$BASEDIR/.extend.vim" ~/.extend.vim
 ln -s "$BASEDIR/.zshrc" ~/.zshrc
